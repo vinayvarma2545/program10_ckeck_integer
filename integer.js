2bf58dbee6c66676d0fe710e1ec81a30099a0367
vinay <vinayvarma2545@gmail.com>
2022-09-21 16:04:52 +0530

    integer

---
+++ integer.js
@@ -0,0 +1,29 @@
+// const value = 0;
+
+// if (value > 0){
+//     console.log(`it is positive integer`)
+// }
+
+// else if (value < 0){
+//     console.log(`it is negative integer`)
+// }
+
+// else {
+//     console.log(`the value is equal to 0`)
+// }
+
+
+let val=0;
+
+if(val>0){
+    console.log(`it is positive integer`)
+
+}
+else if(val==0){
+    console.log(`the value is equal to 0`)
+
+}
+else if(val<0){
+    console.log(`it is negative integer`)
+
+}
